70ca9faf64742aaafbb4d97e5ec297cb698011dc
Nathan Trotter <nathanrtrotter@gmail.com>
2026-02-14 15:14:02 -0700

    Added Movies

--- obj/Debug/net10.0/Mission06_Trotter.AssemblyInfo.cs
+++ obj/Debug/net10.0/Mission06_Trotter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission06_Trotter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03883fc4b53ea92ce6da9f7c8c6da750980baf93")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+789f3db95ee4b01b6332ded16df925a4ef1ec274")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission06_Trotter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission06_Trotter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
